6f5ffee182f559a92a103c862c90ee1f03dfe4c1
ScriptedPranav <sinhafighterpranav@gmail.com>
2024-01-12 21:23:38 +0530

    Get Posts for Users after scraping

--- models.go
+++ models.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/ScriptedPranav/rss-aggregator/internal/database"
@@ -77,4 +78,36 @@ func databaseFeedFollowstoFeedFollows(dbFeedFollows []database.FeedFollow) []Fee
 		feed_follows[i] = databaseFeedFollowToFeedFollow(dbFeedFollow)
 	}
 	return feed_follows
+}
+
+type Post struct {
+	ID          uuid.UUID      `json:"id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	Title       string         `json:"title"`
+	Description sql.NullString `json:"description"`
+	PublishedAt time.Time      `json:"published_at"`
+	Url         string         `json:"url"`
+	FeedID      uuid.UUID      `json:"feed_id"`
+}
+
+func databasePostToPost(dbPost database.Post) Post {
+	return Post{
+		ID:          dbPost.ID,
+		CreatedAt:   dbPost.CreatedAt,
+		UpdatedAt:   dbPost.UpdatedAt,
+		Title:       dbPost.Title,
+		Description: dbPost.Description,
+		PublishedAt: dbPost.PublishedAt,
+		Url:         dbPost.Url,
+		FeedID:      dbPost.FeedID,
+	}
+}
+
+func databasePoststoPosts(dbPosts []database.Post) []Post {
+	posts := make([]Post, len(dbPosts))
+	for i, dbPost := range dbPosts {
+		posts[i] = databasePostToPost(dbPost)
+	}
+	return posts
 }
