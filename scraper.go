1b1388f4b082ade51792a6c6c05907d23a867134
ScriptedPranav <sinhafighterpranav@gmail.com>
2024-01-12 21:02:21 +0530

    save scraped posts to db

--- scraper.go
+++ scraper.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/ScriptedPranav/rss-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(
@@ -17,10 +20,10 @@ func startScraping(
 	log.Printf("Scraping on %v goroutines every %s duration\n", concurrency, timeBetweenRequest)
 	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C {
-		feeds, err := db.GetNextFeedsToFetch(context.Background(),int32(concurrency))
+		feeds, err := db.GetNextFeedsToFetch(context.Background(), int32(concurrency))
 
 		if err != nil {
-			log.Printf("Error fetching feeds %v\n",err)
+			log.Printf("Error fetching feeds %v\n", err)
 			continue
 		}
 
@@ -43,12 +46,40 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 	rssFeed, err := urlToFeed(feed.Url)
 	if err != nil {
-		log.Println("Error fetching feed:",err)
+		log.Println("Error fetching feed:", err)
 		return
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post",item.Title)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.Valid = true
+			description.String = item.Description
+		}
+
+		t, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Println("Error parsing time:", err)
+			continue
+		}
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: t,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+
+		if err != nil {
+			//ignore duplicate key errors because restarting server might cause duplicate posts
+			if strings.Contains(err.Error(), "pq: duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Println("Error creating post:", err)
+		}
 	}
-	log.Printf("Feed %s collected, %v posts found",feed.Name, len(rssFeed.Channel.Item))
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
